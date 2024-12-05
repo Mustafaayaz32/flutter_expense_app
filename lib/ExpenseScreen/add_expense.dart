1c58c0b1d63d73cdbc47bfafc81b3857fecabf28
Mustafaayaz32 <ayazmustafa32@gmail.com>
2024-12-05 19:53:53 +0300

    add expense modal screen

--- lib/ExpenseScreen/add_expense.dart
+++ lib/ExpenseScreen/add_expense.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_app/models/expense_model.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class AddExpenseScreen extends StatefulWidget {
   const AddExpenseScreen({super.key});
@@ -11,6 +13,8 @@ class AddExpenseScreen extends StatefulWidget {
 class _AddExpenseScreenState extends State<AddExpenseScreen> {
   late TextEditingController _expenseNameController;
   late TextEditingController _expenseAmountController;
+  String? selectedDate;
+  Category _selectedCategory = Category.food;
 
   @override
   void initState() {
@@ -26,6 +30,18 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
     super.dispose();
   }
 
+  void _selectDate() async {
+    var date = await showDatePicker(
+        context: context,
+        firstDate: DateTime.now().subtract(const Duration(days: 365)),
+        initialDate: DateTime.now(),
+        lastDate: DateTime.now());
+
+    setState(() {
+      selectedDate = DateFormat('dd/MM/yyyy').format(date!);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -38,7 +54,10 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
           Text(
             'Add a new Expense',
             style: GoogleFonts.openSans(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.w500),
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
           ),
           TextField(
             controller: _expenseNameController,
@@ -50,13 +69,33 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
           const SizedBox(
             height: 10,
           ),
-          TextField(
-            controller: _expenseAmountController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              label: Text('Amount'),
-              prefixText: '\$',
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _expenseAmountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    label: Text('Amount'),
+                    prefixText: '\$',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      selectedDate == null ? 'No date selected' : selectedDate!,
+                    ),
+                    IconButton(
+                      onPressed: _selectDate,
+                      icon: const Icon(Icons.date_range),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
           const SizedBox(
             height: 10,
@@ -67,12 +106,47 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              ElevatedButton(onPressed: () {}, child: const Text('Save')),
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map((category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(
+                            category.name.toUpperCase(),
+                            style: GoogleFonts.openSans(
+                                color: Colors.black, fontSize: 14),
+                          ),
+                        ))
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    if (value == null) {
+                      return;
+                    }
+                    _selectedCategory = value;
+                  });
+                },
+
+                underline: Container(
+                  height: 2,
+                  color:
+                      Theme.of(context).colorScheme.primary, // Underline color
+                ), // Background color of the dropdown
+              ),
+              const Spacer(),
               ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Cancel')),
+                onPressed: () {},
+                child: const Text('Save'),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
             ],
           ),
         ],
